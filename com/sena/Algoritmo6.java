dd0aa22b3f80d521217d9e5e41fe071cbd12e19a
Jm070 <jeancarlospedrozo05@gmail.com>
2022-10-24 11:50:00 -0500

    comentario

---
+++ com/sena/Algoritmo6.java
@@ -0,0 +1,7 @@
+package com.sena;
+
+import javax.swing.*;
+
+public class Algoritmo6 {
+    public static void main(String[] args){
+        
---
+++ com/sena/Algoritmo7.java
@@ -0,0 +1,5 @@
+package com.sena;
+
+public class Algoritmo7 {
+    
+}
